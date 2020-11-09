5e2ea955731ade6b148a0e96a87db3c877ebbbe0
Sam Slotsky <sam.slotsky@gatsbyjs.com>
2020-11-09 12:26:44 -0600

    Expose plugin-specific refresh endpoint in gatsby-serve

--- packages/gatsby/src/utils/start-server.ts
+++ packages/gatsby/src/utils/start-server.ts
@@ -190,11 +190,30 @@ export async function startServer(
    * If no GATSBY_REFRESH_TOKEN env var is available, then no Authorization header is required
    **/
   const REFRESH_ENDPOINT = `/__refresh`
-  const refresh = async (req: express.Request): Promise<void> => {
+  const refresh = async (
+    req: express.Request,
+    pluginName?: string
+  ): Promise<void> => {
     emitter.emit(`WEBHOOK_RECEIVED`, {
       webhookBody: req.body,
+      pluginName,
     })
   }
+
+  app.post(`${REFRESH_ENDPOINT}/:plugin_name`, (req, res) => {
+    const pluginName = req.params[`plugin_name`]
+
+    const enableRefresh = process.env.ENABLE_GATSBY_REFRESH_ENDPOINT
+    const refreshToken = process.env.GATSBY_REFRESH_TOKEN
+    const authorizedRefresh =
+      !refreshToken || req.headers.authorization === refreshToken
+
+    if (enableRefresh && authorizedRefresh) {
+      refresh(req, pluginName)
+    }
+    res.end()
+  })
+
   app.use(REFRESH_ENDPOINT, express.json())
   app.post(REFRESH_ENDPOINT, (req, res) => {
     const enableRefresh = process.env.ENABLE_GATSBY_REFRESH_ENDPOINT
